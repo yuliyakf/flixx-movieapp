972ea0eb5daf932c775bc2e64dc3db929051b258
Yuliya <yuliyakf@gmail.com>
2023-06-21 21:41:17 -0400

    added popular movies

--- js/script.js
+++ js/script.js
@@ -7,6 +7,49 @@ const global ={
 }
 //console.log(global.currentPage)
 
+async function displayPopularMovies(){
+    const  { results }  = await fetchAPIData('movie/popular'); //passing endpoint here
+    
+    results.forEach(movie => {
+        const div= document.createElement('div');
+        div.classList.add('card'); 
+        //checking for movie img in data if exists fetch else no-image 
+        div.innerHTML = ` 
+            <a href='movie-details.html?id=${movie.id}'>
+           ${movie.poster_path 
+            ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+           class="card-img-top"
+           alt="${movie.title}"
+           />` 
+           : `<img src="../images/no-image.jpg"
+           class="card-img-top"
+           alt="${movie.title}"
+           />`
+            }
+            </a>
+            <div class= "card-body">
+            <h5 class= "card-title">${movie.title}</h5>
+            <p class= "card-text">
+            <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+            </div>
+        `;
+
+        document.querySelector('#popular-movies').appendChild(div);
+    });
+}
+
+//Fetch data from TMDB API
+async function fetchAPIData(endpoint){
+    const API_KEY = '89b065f503c852471b7275e86922c78e';
+    const API_URL = 'https://api.themoviedb.org/3/';
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+
+    const data= await response.json();
+    return data;
+}
+
 //Highlight active link
 function highlightActiveLink(){
     const links= document.querySelectorAll('.nav-link');
@@ -22,7 +65,7 @@ function init(){  //init works on every page
     switch (global.currentPage){
         case '/':
         case '/index.html':
-            console.log('Home');
+            displayPopularMovies();
             break;
         case '/shows.html':
             console.log('shows');
